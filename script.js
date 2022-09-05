e32da91662dfd71451468f06d5e98f8467dbc8d3
naThan251 <97640857+naThan251@users.noreply.github.com>
2022-09-05 15:15:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstName = "Nathan "
+
+let secondName = "Ernane"
+
+let name = firstName.concat(secondName)
+
+let a = 10
+
+let b = 5
+
+let AmaisB = a+b;
+
+let AmenosB = a-b;
+
+let AvezesB = a*b;
+
+let AdivididoB = a/b;
+
+console.log(firstName)
+console.log(secondName)
+console.log(name)
+
+console.log(a)
+console.log(b)
+console.log(AmaisB)
+console.log(AmenosB)
+console.log(AvezesB)
+console.log(AdivididoB)
+
